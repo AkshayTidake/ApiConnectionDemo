46a0978d2118b22bebed884a71c0b627c0a1508c
AkshayTidake <akshaytidake0918@gmail.com>
2016-10-15 15:46:45 +0530

    Initial commit

---
+++ app/src/main/java/adservices/demoproject/MainActivity.java
@@ -0,0 +1,125 @@
+package adservices.demoproject;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,View.OnClickListener{
+   private static int   SIGN_IN=0;
+    private GoogleApiClient ac;
+    private FirebaseAuth fa;
+    private FirebaseAuth.AuthStateListener mauth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fa=FirebaseAuth.getInstance();
+        mauth=new FirebaseAuth.AuthStateListener(){
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Toast.makeText(MainActivity.this,"successfull"+user.getEmail(),Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"sign in error",Toast.LENGTH_LONG).show();
+                }
+                }
+            };
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        ac=new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API)
+                                                .build();
+                    findViewById(R.id.gid).setOnClickListener(this);
+
+
+        }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fa.addAuthStateListener(mauth);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mauth!=null){
+            fa.removeAuthStateListener(mauth);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==SIGN_IN){
+            GoogleSignInResult gsr=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(gsr.isSuccess()){
+                GoogleSignInAccount gsa=gsr.getSignInAccount();
+                firebaseAuthWithGoogle(gsa);
+
+            }
+            else {
+                Log.d("sign in","failed");
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount ga){
+
+        AuthCredential acr= GoogleAuthProvider.getCredential(ga.getIdToken(),null);
+        fa.signInWithCredential(acr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("auth","complete"+task.isSuccessful());
+            }
+        });
+    }
+    private void signIn(){
+        Intent i=Auth.GoogleSignInApi.getSignInIntent(ac);
+        startActivityForResult(i,SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+       if(view.getId()==R.id.gid) {
+           signIn();
+       }
+    }
+}
